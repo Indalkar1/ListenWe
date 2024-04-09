b8bbab2ee0d4c30f5987239fd46f7263c71dfbac
SamikshaI <indalkarsamiksha@gmail.com>
2024-04-09 17:40:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css"
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import {useState} from "react";
+import useClipboard from "react-use-clipboard";
+
+const App = () => {
+  const { transcript, resetTranscript,browserSupportsSpeechRecognition } = useSpeechRecognition();
+    const [textToCopy, setTextToCopy] = useState();
+    const [isCopied, setCopied] = useClipboard(transcript);
+    const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
+    if (!browserSupportsSpeechRecognition) {
+        return null
+    }
+
+    return (
+        <>
+            <div className="container">
+                <h2>Speech to Text Converter</h2>
+                <br/>
+                <p>Just clik on start listening and 🔊🔊🔊🔊 then click on copy button to copy text</p>
+
+                <div className="main-content">
+                    {transcript}
+                </div>
+
+                <div className="btn-style">
+                    <button onClick={setCopied}>
+                       {isCopied ? "Copied" : "Copy"}
+                    </button>
+                    <button onClick={startListening}>Start Listening</button>
+                    <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
+                    <button onClick={resetTranscript}>Reset</button>
+
+
+                </div>
+
+            </div>
+
+        </>
+    );
+};
 
 export default App;
